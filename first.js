5f5818fa86993109c14255cce3dccfe7b3a46ebc
Dylan Kuo <dylankuo98@gmail.com>
2023-04-05 14:54:01 -0700

    Link external js script and create getComputerChoice function

---
+++ first.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() *3)
+    switch (randomNumber) {
+        case 0:
+            return 'ROCK'
+        case 1:
+            return 'PAPER'
+        case 2:
+            return 'SCISSORS'
+    }
+}
+console.log(getComputerChoice(3));
