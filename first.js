23cc2d20e9577b196815b4fab923dca31eceb88c
Dylan Kuo <dylankuo98@gmail.com>
2023-04-05 16:58:03 -0700

    Write a function that plays one round of Rock Paper Scissors

--- first.js
+++ first.js
@@ -9,4 +9,25 @@ function getComputerChoice() {
             return 'SCISSORS'
     }
 }
-console.log(getComputerChoice(3));
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return("It's a tie! You both picked ${playerSelection}");
+    } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+        return("You Win! Rock beats Scissors");
+    } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+        return("You Win! Paper beats Rock");
+    } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return("You Win! Scissors beats Paper");
+    } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+        return("You Lose! Rock beats Scissors");
+    } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+        return("You Lose! Scissors beats Paper");
+    } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+        return("You Lose! Paper beats Rock");
+    }
+}
+
+const playerSelection = prompt('Rock, Paper, Scissors').toUpperCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
