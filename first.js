27759deec25b965cbe9208a1a048b6b4abd956dc
Dylan Kuo <dylankuo98@gmail.com>
2023-04-05 18:39:45 -0700

    Condense game function lines

--- first.js
+++ first.js
@@ -12,22 +12,17 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
-        return("It's a tie! You both picked ${playerSelection}");
-    } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-        return("You Win! Rock beats Scissors");
-    } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        return("You Win! Paper beats Rock");
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return("You Win! Scissors beats Paper");
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        return("You Lose! Rock beats Scissors");
-    } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        return("You Lose! Scissors beats Paper");
-    } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        return("You Lose! Paper beats Rock");
+        return("It's a tie!");
+    } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return("You Win!");
+    } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK' || playerSelection === 'PAPER' && computerSelection === 'SCISSORS' || playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+        return("You Lose!");
     }
 }
 
-const playerSelection = prompt('Rock, Paper, Scissors').toUpperCase();
-const computerSelection = getComputerChoice();
+
+let playerSelection = prompt('Rock, Paper, Scissors').toUpperCase();
+let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+let playerScore = 0;
+let computerScore = 0;
